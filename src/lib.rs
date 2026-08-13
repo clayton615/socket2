db5eff69b7efb3ea9267b1fc4d6b2d8ff4d8e8a6
Clayton Slaughter <clay.slaughter@ibm.com>
2026-08-13 15:41:34 -0500

    Add support for AF_IUCV sockets
    * changed doc comment to be above the cfg guard
    * removed `unneeded unsafe_op_in_unsafe_fn`
    * removed `as_socket_iucv()` function

--- src/lib.rs
+++ src/lib.rs
@@ -224,6 +224,14 @@ impl Domain {
     /// Domain for IPv6 communication, corresponding to `AF_INET6`.
     pub const IPV6: Domain = Domain(sys::AF_INET6);
 
+    /// Domain for IUCV socket communication, corresponding to `AF_IUCV`.
+    #[cfg(all(
+        feature = "all",
+        any(target_arch = "x86_64", target_arch = "s390x"),
+        target_env = "gnu"
+    ))]
+    pub const IUCV: Domain = Domain(sys::AF_IUCV);
+
     /// Domain for Unix socket communication, corresponding to `AF_UNIX`.
     #[cfg(not(target_os = "wasi"))]
     pub const UNIX: Domain = Domain(sys::AF_UNIX);
